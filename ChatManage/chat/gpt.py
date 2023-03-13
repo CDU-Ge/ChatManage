897d6e2919212822badb63f314e0a62c8a51bda1
VincentGe <cosplox@outlook.com>
2023-03-13 16:09:21 +0800

    优化服务

--- ChatManage/chat/gpt.py
+++ ChatManage/chat/gpt.py
@@ -5,15 +5,17 @@
 
 """
 
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
 from __future__ import absolute_import
 from __future__ import annotations
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
+import logging
 import typing as t
-import importlib
+
 from django.db import transaction
+
 from .models import ApiKey
 
 
@@ -34,8 +36,6 @@ def question(qs: t.List[str], api_key: str) -> t.Generator[str]:
     # openai = importlib.import_module('openai')
     import openai
     openai.api_key = api_key
-    print(f'question key: {api_key}')
-    openai.proxy = 'http://127.0.0.1:10809'
     try:
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
@@ -45,6 +45,7 @@ def question(qs: t.List[str], api_key: str) -> t.Generator[str]:
             if char := resp.get("content"):
                 yield char
     except Exception as e:
+        logging.warning(e)
         with transaction.atomic():
             key = ApiKey.objects.filter(value=api_key).first()
             key.is_valid = False
@@ -55,31 +56,3 @@ def question(qs: t.List[str], api_key: str) -> t.Generator[str]:
             key = ApiKey.objects.filter(value=api_key).first()
             key.is_work = False
             key.save()
-
-
-if __name__ == '__main__':
-    with open(r"H:\Projects\ChatManage\r.txt", 'r', encoding='utf8') as f:
-        content = f.readlines()
-    record = {'qs': []}
-    buff = []
-    for i in content:
-        if i.startswith(('# ', 'assistant')):
-            if buff:
-                record['qs'].append(''.join(buff))
-                buff.clear()
-            i = i.replace('# ', '').replace('assistant', '').strip()
-        buff.append(i)
-    print(len(''.join(record['qs'])))
-    # with open('record.json', 'w', encoding='utf8') as f:
-    #     import json
-    #
-    #     json.dump(record, f)
-    _qs = []
-    # while 1:
-    #     prompt = input('# ')
-    #     if prompt == 'reset':
-    #         _qs.clear()
-    #     if prompt == 'quit':
-    #         break
-    #     _qs.append(prompt)
-    #     question(_qs, "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")
